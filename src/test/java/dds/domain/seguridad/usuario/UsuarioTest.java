ced7003fce4ed5cb188c76e927879c094f762d08
mlanneponders <mlyonadi@gmail.com>
2021-05-01 12:43:28 -0300

    Creo test de usuario

---
+++ src/test/java/dds/domain/seguridad/usuario/UsuarioTest.java
@@ -0,0 +1,12 @@
+package dds.domain.seguridad.usuario;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UsuarioTest {
+
+    @Test
+    public void lastPasswords() {
+    }
+}
