d002c080f7a099da08d94bf52bb58790a1c45230
Dorr Pedro <pdorr@utn.frba.edu.ar>
2021-05-06 21:48:14 -0300

    agrego api wpp

--- src/main/java/dds/servicios/avisos/AdapterWhatsApp.java
+++ src/main/java/dds/servicios/avisos/AdapterWhatsApp.java
@@ -1,8 +1,26 @@
 package dds.servicios.avisos;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
-public class AdapterWhatsApp {
+public class AdapterWhatsApp implements AdapterFormaNotificacion{
+
     @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }g
+    public void notificar(String mensaje, Contacto contacto) {
+        // Find your Account Sid and Token at twilio.com/console
+        // and set the environment variables. See http://twil.io/secure
+        String ACCOUNT_SID = System.getenv("AC0e930b6c51a527c0a83a7bbe5fe4ee80");
+        String AUTH_TOKEN = System.getenv("07cf1b839c8bfda71bedb2e458da9ed6");
+
+
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("whatsapp:+5491140435092"),
+                new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+                mensaje)
+                .create();
+
+        System.out.println(message.getSid());
+
+    }
 }
--- src/test/java/dds/servicios/avisos/NotificadorTest.java
+++ src/test/java/dds/servicios/avisos/NotificadorTest.java
@@ -9,6 +9,7 @@ public class NotificadorTest extends TestCase {
     Notificador noti = new Notificador();
     AdapterSMS testeoSMS = new AdapterSMS();
     AdapterSMS testeoSMS2 = new AdapterSMS();
+    AdapterWhatsApp testeoWPP = new AdapterWhatsApp();
     List<AdapterFormaNotificacion> formasDeNoti = new ArrayList();
 
 
@@ -18,5 +19,12 @@ public class NotificadorTest extends TestCase {
     noti.agendarContacto("pedro","dorr","+541155892198","dorrpei@gmail.com",formasDeNoti);
     noti.agendarContacto("pedro","dorr","+541140435092","dorrpei@gmail.com",formasDeNoti);
     noti.notificar("probando multiple mensaje");
+    }
+@Test
+    public void testNotificarWPP() {
+        formasDeNoti.add(testeoWPP);
+        //noti.agendarContacto("pedro","dorr","+541155892198","dorrpei@gmail.com",formasDeNoti);
+        noti.agendarContacto("pedro","dorr","+541140435092","dorrpei@gmail.com",formasDeNoti);
+        noti.notificar("probando wpp");
     }
 }
