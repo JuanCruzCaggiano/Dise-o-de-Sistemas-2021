20b5aa7dadadf7add034535e9111436dc2ef589f
Dorr Pedro <pdorr@utn.frba.edu.ar>
2021-05-07 17:12:51 -0300

    hay que reveer bien que va aca

---
+++ src/main/java/dds/domain/asociacion/ConfigCaracMascota.java
@@ -0,0 +1,20 @@
+package dds.domain.asociacion;
+
+import java.util.List;
+
+public class ConfigCaracMascota implements Configuracion {
+    private List<String> caracteristicas;
+
+    @Override
+    public void configurar() {
+
+    }
+    public void agregarCaracteristica(){
+
+    }
+    public void crearCaracteristica(){
+
+    }
+
+
+}
--- src/main/java/dds/domain/seguridad/usuario/Administrador.java
+++ src/main/java/dds/domain/seguridad/usuario/Administrador.java
@@ -1,11 +1,24 @@
 package dds.domain.seguridad.usuario;
 
+import dds.domain.asociacion.Configuracion;
+
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
-public class Administrador extends Usuario{
+public class Administrador extends Usuario implements Configuracion {
 
-    public Administrador(String userName,String passWord) throws NoSuchAlgorithmException {
+    private final List<Configuracion> configuracion;
+
+
+    public Administrador(String userName,String passWord,List<Configuracion> config) throws NoSuchAlgorithmException {
         super(userName,passWord);
+        this.configuracion = config;
+
+    }
+
+
+    @Override
+    public void configurar() {
 
     }
 }
