d35a2c7bffd8072a48c9072d3e29ef2da8092134
Dorr Pedro <pdorr@utn.frba.edu.ar>
2021-05-09 21:41:50 -0300

    Repo Personas hasta que persistamos

---
+++ src/main/java/dds/domain/persona/RepositorioPersonas.java
@@ -0,0 +1,47 @@
+package dds.domain.persona;
+
+
+
+import dds.domain.mascota.Mascota;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioPersonas {
+    private List<Persona> personas;
+
+    public void agregarPersona(Persona pppl) {
+        if (personas ==null)	{
+            personas = new ArrayList<Persona>();
+            personas.add(pppl);}
+        else {
+            personas.add(pppl);
+        }
+    }
+    public void remover(Persona aRemover){
+        personas.remove(buscarPersona(aRemover));
+
+    }
+    public List<Persona> getRepoPersonas() {
+        return personas;
+    }
+
+    public int buscarPersona(Persona buscada){   //TODO cuando persistamos buscamos por ID y no por nombre
+        for (int i = 0; i< personas.size(); i++){
+            if (buscada.getNombre() == personas.get(i).getNombre()){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public int tieneMascota(Mascota mascota){  //TODO mismo que arriba, esto se busca por id  /esto devuelve de quien es la mascota
+        for (int i = 0; i< personas.size(); i++){
+            for (int j = 0; j< personas.get(i).getMascotas().size(); j++) {
+                if (mascota.getNombre() == personas.get(i).getMascotas().get(j).getNombre()) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
