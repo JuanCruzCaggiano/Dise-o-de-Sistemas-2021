d25750d4b419f1538e38024ff481dd1c922e2c91
gnfigueroa <gafigueroa@est.frba.utn.edu.ar>
2021-06-06 01:16:38 -0300

    Cambios en api rest

--- src/main/java/dds/servicios/apiHogares/HogarDeTransito.java
+++ src/main/java/dds/servicios/apiHogares/HogarDeTransito.java
@@ -1,2 +1,119 @@
-package dds.servicios.apiHogares;public class HogarDeTransito {
+package dds.servicios.apiHogares;
+
+
+public class HogarDeTransito {
+    private String total;
+    private String offset;
+    private Ubicacion ubicacion;
+    private String telefono;
+    private Admision admision;
+    private String id;
+    private String nombre;
+    private int capacidad;
+    private int lugares_disponibles;
+    private Boolean patio;
+    private String caracteristicas;
+
+
+    public HogarDeTransito(String total, String offset, Ubicacion ubicacion, String telefono, Admision admision, String id, String nombre, int capacidad, int lugares_disponibles, Boolean patio, String caracteristicas) {
+        this.total = total;
+        this.offset = offset;
+        this.ubicacion = ubicacion;
+        this.telefono = telefono;
+        this.admision = admision;
+        this.id = id;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.lugares_disponibles = lugares_disponibles;
+        this.patio = patio;
+        this.caracteristicas = caracteristicas;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getOffset() {
+        return offset;
+    }
+
+    public void setOffset(String offset) {
+        this.offset = offset;
+    }
+
+    public Ubicacion getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(Ubicacion ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Admision getAdmision() {
+        return admision;
+    }
+
+    public void setAdmision(Admision admision) {
+        this.admision = admision;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getLugares_disponibles() {
+        return lugares_disponibles;
+    }
+
+    public void setLugares_disponibles(int lugares_disponibles) {
+        this.lugares_disponibles = lugares_disponibles;
+    }
+
+    public Boolean getPatio() {
+        return patio;
+    }
+
+    public void setPatio(Boolean patio) {
+        this.patio = patio;
+    }
+
+    public String getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    public void setCaracteristicas(String caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
 }
--- src/main/java/dds/servicios/apiHogares/Ubicacion.java
+++ src/main/java/dds/servicios/apiHogares/Ubicacion.java
@@ -1,2 +1,38 @@
-package dds.servicios.apiHogares;public class Ubicacion {
+package dds.servicios.apiHogares;
+
+public class Ubicacion {
+    private String direccion;
+    private int lat;
+    private int longitud;
+
+
+    public Ubicacion(String direccion, int lat, int longitud) {
+        this.direccion = direccion;
+        this.lat = lat;
+        this.longitud = longitud;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getLat() {
+        return lat;
+    }
+
+    public void setLat(int lat) {
+        this.lat = lat;
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
 }
