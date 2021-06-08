11358a5ef050d653bee63a0a7fa1246e93f171dc
mlanneponders <mlyonadi@gmail.com>
2021-06-07 22:04:34 -0300

    Rectifico repos

--- src/main/java/dds/domain/persona/Rescatista.java
+++ src/main/java/dds/domain/persona/Rescatista.java
@@ -14,7 +14,7 @@ public class Rescatista implements RolPersona {
 
     public void encontreMascotaPerdida(Mascota mascota, RepositorioPersonas repo) throws MessagingException { //esto busca al dueño y dentro del repo se encarga de notificar
         Integer identificador = repo.tieneMascota(mascota);
-        repo.getRepositorio().get(identificador).getNotificador().notificar("encontramos a "+mascota.getNombre());
+        RepositorioPersonas.getRepositorio().getPersonas().get(identificador).getNotificador().notificar("encontramos a "+mascota.getNombre());
     }
     private void buscarHogarDeTransito(){ //TODO falta implementar
 
