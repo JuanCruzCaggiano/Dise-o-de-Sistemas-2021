2f716a0eec17c808fc760db3cc100bc6e93d8b36
mlanneponders <mlyonadi@gmail.com>
2021-06-07 22:52:06 -0300

    Creo estructura funciones de encontreMascotaPerdida

--- src/main/java/dds/domain/persona/Rescatista.java
+++ src/main/java/dds/domain/persona/Rescatista.java
@@ -3,6 +3,7 @@ package dds.domain.persona;
 import dds.db.RepositorioPersonas;
 import dds.domain.mascota.Mascota;
 import dds.servicios.apiHogares.Ubicacion;
+import dds.servicios.publicaciones.PublicacionMascota;
 
 import javax.mail.MessagingException;
 import java.util.ArrayList;
@@ -17,12 +18,14 @@ public class Rescatista implements RolPersona {
     public void puencontreMascotaPerdida(float latitud, float longitud, ArrayList<String> listaFotos,String descripcion){
 
     }
-    public void encontreMascotaPerdida(int idMascota, float latitud, float longitud, ArrayList<String> listaFotos,String descripcion){
+    public void encontreMascotaPerdida(String idMascota, float latitud, float longitud, ArrayList<String> listaFotos,String descripcion){
+        new PublicacionMascota(idMascota,latitud,longitud,listaFotos,descripcion);
 
     }
 
     public void encontreMascotaPerdida(Mascota mascota, RepositorioPersonas repo) throws MessagingException { //esto busca al dueño y dentro del repo se encarga de notificar
         Integer identificador = repo.tieneMascota(mascota);
+        new
         RepositorioPersonas.getRepositorio().getPersonas().get(identificador).getNotificador().notificar("encontramos a "+mascota.getNombre());
     }
     private void buscarHogarDeTransito(){ //TODO falta implementar
