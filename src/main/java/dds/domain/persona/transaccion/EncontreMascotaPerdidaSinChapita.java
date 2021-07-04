62719f2566eda14a39eac1d85c54ba2fc5bc9e28
mlanneponders <mlyonadi@gmail.com>
2021-07-04 16:41:29 -0300

    Corrijo idTransaccion

--- src/main/java/dds/domain/persona/transaccion/EncontreMascotaPerdidaSinChapita.java
+++ src/main/java/dds/domain/persona/transaccion/EncontreMascotaPerdidaSinChapita.java
@@ -10,7 +10,7 @@ import dds.servicios.publicaciones.PublicacionMascota;
 import java.util.ArrayList;
 
 public class EncontreMascotaPerdidaSinChapita implements Transaccion{
-    final  int idTransaccion = 4;
+    final  int idTransaccion = 9;
     String idMascota;
     float latitud;
     float longitud;
