c4eeca71b0acb14a648d8d92744eef1ea00cb885
mlanneponders <mlyonadi@gmail.com>
2021-09-07 18:19:48 -0300

    Persistencia de testeo

--- src/test/java/dds/domain/asociacion/ConfiguracionesTest.java
+++ src/test/java/dds/domain/asociacion/ConfiguracionesTest.java
@@ -9,6 +9,7 @@ import dds.domain.persona.TipoDocumento;
 import dds.domain.persona.roles.Duenio;
 import dds.domain.seguridad.usuario.Administrador;
 import dds.domain.seguridad.usuario.Standard;
+import dds.domain.seguridad.usuario.Usuario;
 import dds.servicios.apiHogares.Ubicacion;
 import dds.servicios.avisos.AdapterEmail;
 import dds.servicios.avisos.AdapterFormaNotificacion;
@@ -25,39 +26,42 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-//CORRER Test x test
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class ConfiguracionesTest  extends TestCase{
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ConfiguracionesTest {
+    Usuario usuDuenio;
     String fotoAModif = "imgprueba.jpg";
     String fotoModif = "recorte.jpg";
     Asociacion asoc;
     Mascota mascota;
     Persona personaDuenio;
-    Standard usuDuenio;
+
     @Before
     public void setUp() throws NoSuchAlgorithmException {
-        //CREO ASOC
-        asoc = new Asociacion("Asco",new Ubicacion("DIR",0,0));
-        mascota = new Mascota(TipoMascota.PERRO,"lola beatriz","lola", LocalDate.now().minusYears(6),"bruta beba",new ArrayList<>(),new HashMap<>(), Sexo.MACHO);
-        //CREO DUENIO
-        AdapterEmail adEmail = new AdapterEmail();
-        List<AdapterFormaNotificacion> formasDeNoti = new ArrayList<>();
-        formasDeNoti.add(adEmail);
-        personaDuenio = new Persona("Matias", "Lanneponders", TipoDocumento.DNI,
-                39000401,LocalDate.of(1995, 7, 7),
-                "dir","1155892198", "mlyonadi@gmail.com", formasDeNoti);
-        personaDuenio.agregarRol(Duenio.getDuenio());
-        personaDuenio.getMascotas().add(mascota);
-        usuDuenio = new Standard("matilanne","Password1234+",personaDuenio);
-        usuDuenio.setAsociacion(asoc);
-        if (EntityManagerHelper.getEntityManager().find(Asociacion.class, 1) == null){
+
+        if (EntityManagerHelper.getEntityManager().createQuery("from Asociacion").getResultList().isEmpty()){
+            //CREO ASOC
+            asoc = new Asociacion("Asco",new Ubicacion("DIR",0,0));
+            mascota = new Mascota(TipoMascota.PERRO,"lola beatriz","lola", LocalDate.now().minusYears(6),"bruta beba",new ArrayList<>(),new HashMap<>(), Sexo.MACHO);
+            //CREO DUENIO
+            AdapterEmail adEmail = new AdapterEmail();
+            List<AdapterFormaNotificacion> formasDeNoti = new ArrayList<>();
+            formasDeNoti.add(adEmail);
+            personaDuenio = new Persona("Matias", "Lanneponders",TipoDocumento.DNI,
+                    39000401,LocalDate.of(1995, 7, 7),
+                    "dir","1155892198", "mlyonadi@gmail.com", formasDeNoti);
+
+            usuDuenio = new Standard("matilanne","Password1234+",personaDuenio);
+            usuDuenio.setAsociacion(asoc);
+            personaDuenio.agregarRol(Duenio.getDuenio());
+            personaDuenio.getMascotas().add(mascota);
+
             EntityManagerHelper.beginTransaction();
             EntityManagerHelper.entityManager().persist(asoc);
-            asoc.getConfiguraciones().agregarCaracteristicaMascota("Color de Pelo");
-            asoc.getConfiguraciones().agregarCaracteristicaMascota("Tamaño");
             EntityManagerHelper.entityManager().persist(usuDuenio);
             EntityManagerHelper.commit();
+            asoc.getConfiguraciones().agregarCaracteristicaMascota("Color de Pelo");
+            asoc.getConfiguraciones().agregarCaracteristicaMascota("Tamaño");
         } else
         {
             asoc = (Asociacion) EntityManagerHelper.getEntityManager().createQuery("from Asociacion ").getResultList().get(0);
@@ -65,11 +69,9 @@ public class ConfiguracionesTest  extends TestCase{
         }
     }
 
-
-
-
     @Test
     public void testFoto() {
+
         asoc.getConfiguraciones().cambiarTamanio(fotoAModif,fotoModif);
     }
     @Test
