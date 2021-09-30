b150808d8e4a29f2ea291ab8cbbe1a67ad5060f4
Pdorr <dorrsito@gmail.com>
2021-09-30 17:53:46 -0300

    commit generico

---
+++ src/main/java/dds/domain/controllers/AdopcionController.java
@@ -0,0 +1,52 @@
+package dds.domain.controllers;
+
+
+import dds.db.RepositorioMascotas;
+import dds.domain.entities.mascota.Mascota;
+import dds.domain.entities.mascota.Sexo;
+import dds.domain.entities.mascota.TipoMascota;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+import java.io.Console;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class AdopcionController {
+    private RepositorioMascotas repositorio;
+
+    public AdopcionController(){this.repositorio=RepositorioMascotas.getRepositorio();}
+
+
+    public ModelAndView mostrarMascotas(Request req,Response rep){
+
+        Map<String,Object> parametros = new HashMap<>();
+        System.out.print("asdd1");
+        List<Mascota> mascotas = this.repositorio.getMascotas();
+
+        System.out.print("asdd");
+        parametros.put("mascotas",mascotas);
+
+        return new ModelAndView(parametros,"adopcion.hbs");
+    }
+    public ModelAndView mascotaId(Request req,Response rep){
+        //validar que exista el id
+        Mascota mascota = this.repositorio.getMascota(new String(req.params("id")));//o lo que sea el id
+        Map<String,Object> parametros = new HashMap<>();
+        parametros.put("mascotas",mascota);
+        return new ModelAndView(parametros,"mascota.hbs"); //TODO: crear ventana que muestre 1 sola mascota por id con datos de contacto
+        // {{#if mascota}} value = "{{mascota.apodo}}" {{/if}}  esto muestras el apodo de la mascota en el .hbs
+    }
+/* ESTO VA EN MASCOTA CONTROLLER y va en un spark.post
+    public Response mascotaId(Request req,Response rep){
+        //validar que exista el id
+        Mascota mascota = this.repositorio.getMascota(new String(req.params("id")));//o lo que sea el id
+        String nuevoNombre = request.queryParams("nombre"); //meter try catch y lo que va en el queryparams tiene que coincidir con el input del html
+        mascota.setNombre(nuevoNombre);
+    }*/
+}
---
+++ src/main/java/dds/server/Router.java
@@ -0,0 +1,43 @@
+package dds.server;
+
+
+import dds.domain.controllers.AdopcionController;
+import dds.domain.controllers.UsuarioController;
+
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import dds.spark.utils.BooleanHelper;
+import dds.spark.utils.HandlebarsTemplateEngineBuilder;
+
+public class Router {
+    private static HandlebarsTemplateEngine engine;
+
+    private static void initEngine() { //construimos instancia de hbs
+        Router.engine = HandlebarsTemplateEngineBuilder
+                .create()
+                .withDefaultHelpers()
+                .withHelper("isTrue", BooleanHelper.isTrue)
+                .build();
+    }
+
+    public static void init() {
+        Router.initEngine();
+        Spark.staticFileLocation("/public");
+        Router.configure();
+    }
+
+    private static void configure(){
+        //Spark.get("/",(req,res)->"hola");  //+ req.queryParams("nombre") + " " +req.queryParams("apellido")
+        // localhost:9000/?nombre=sarasa&apellido=sarasa2
+
+        //Spark.get("/usuario/:id", (req,res)->req.params("id"));  //req.splat()[0] devuelve array de asteriscos
+        UsuarioController usuarioController = new UsuarioController();
+        Spark.get("/saludar",usuarioController::mostrarComandera, Router.engine); //usa :: para invocar al metodo pero desde la ruta y no desde el router
+
+        AdopcionController adopcionController = new AdopcionController();
+        Spark.get("/adopcion",adopcionController::mostrarMascotas, Router.engine);
+        Spark.get("/adopcion/:id",adopcionController::mostrarMascotas, Router.engine);
+
+    }
+
+}
