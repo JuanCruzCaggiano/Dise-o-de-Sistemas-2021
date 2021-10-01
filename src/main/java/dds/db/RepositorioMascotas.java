8c3040c8f910b3df079f53d58bc0006599a5a7ef
Pdorr <dorrsito@gmail.com>
2021-09-30 21:54:56 -0300

    modifico entidad para que permita buscar las mascotas en adopcion

--- src/main/java/dds/db/RepositorioMascotas.java
+++ src/main/java/dds/db/RepositorioMascotas.java
@@ -3,7 +3,9 @@ package dds.db;
 
 import dds.db.repositorioException.LogicRepoException;
 import dds.domain.entities.mascota.Mascota;
+import org.apache.commons.lang3.ObjectUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RepositorioMascotas {
@@ -22,6 +24,13 @@ public class RepositorioMascotas {
         }else{
             throw new LogicRepoException("Id Mascota Inexistente");
         }
+    }
+    public List<Mascota> getMascotasPorListaId(List<String>ids) {
+        List<Mascota> mascotas = new ArrayList<>();
+        for (String id:ids){
+            mascotas.add(this.getMascota(id));
+        }
+        return mascotas;
     }
 
     public boolean esIDValido(String ID) {
