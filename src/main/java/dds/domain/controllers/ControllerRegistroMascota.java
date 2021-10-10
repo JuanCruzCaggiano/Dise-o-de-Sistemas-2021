7cd744c8ef3cff02bd9b34b45c1745f85d8c1354
Pdorr <dorrsito@gmail.com>
2021-10-10 18:24:31 -0300

    agrego lo faltante para registrar mascota

--- src/main/java/dds/domain/controllers/ControllerRegistroMascota.java
+++ src/main/java/dds/domain/controllers/ControllerRegistroMascota.java
@@ -1,6 +1,12 @@
 package dds.domain.controllers;
 
 import dds.db.RepositorioUsuarios;
+import dds.domain.entities.asociacion.Asociacion;
+import dds.domain.entities.mascota.Mascota;
+import dds.domain.entities.mascota.Sexo;
+import dds.domain.entities.mascota.TipoMascota;
+import dds.domain.entities.persona.TipoDocumento;
+import dds.domain.entities.persona.transaccion.RegistrarMascota;
 import dds.domain.entities.seguridad.usuario.Standard;
 import dds.domain.entities.seguridad.usuario.Usuario;
 import spark.ModelAndView;
@@ -8,7 +14,11 @@ import spark.Request;
 import spark.Response;
 
 import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ControllerRegistroMascota {
@@ -16,15 +26,32 @@ public class ControllerRegistroMascota {
     }
 
     public ModelAndView registrarMascota(Request request, Response response) throws NoSuchAlgorithmException {
+        Usuario usuario = request.session().attribute("usuario");
+        Asociacion asoc = usuario.getAsociacion();
+        String tipo = (request.queryParams("tipo") != null) ? request.queryParams("tipo") : "";
         String nombre = (request.queryParams("nombre") != null) ? request.queryParams("nombre") : "";
-        String raza = (request.queryParams("raza") != null) ? request.queryParams("raza") : "";
         String desc = (request.queryParams("desc") != null) ? request.queryParams("desc") : "";
-        //TODO: Falta ampliar la vista para poder poner todos los atributos de una mascota el resto es similar a lo del registro usuario
-
+        String apodo  = (request.queryParams("apodo") != null) ? request.queryParams("apodo") : "";
+        String sexo = (request.queryParams("sexo") != null) ? request.queryParams("sexo") : "";
+        String caracs = (request.queryParams("caracteristicas") != null) ? request.queryParams("caracteristicas") : "";
+        String foto = (request.queryParams("foto") != null) ? request.queryParams("foto") : "";
+        String fecha = (request.queryParams("fecha") != null) ? request.queryParams("fecha") : "";
+        List<String> fotos = new ArrayList<>(); // TODO: Manejo de fotos
+        Sexo sex= Sexo.valueOf(sexo);
+        TipoMascota tip = TipoMascota.valueOf(tipo);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate dt = LocalDate.parse(fecha,dtf);
+        HashMap <String, String> caracteristica = new HashMap<>();
+        List<String> preguntasCaracs = asoc.getConfigurador().getClaves();
+        for (int i =0 ;i<preguntasCaracs.size();i++){
+            caracteristica.put(preguntasCaracs.get(i),(request.queryParams(preguntasCaracs.get(i)) != null) ? request.queryParams(preguntasCaracs.get(i)) : "");
+        }
+        //Mascota mascota = new Mascota(tip,nombre,apodo,dt,desc,fotos,caracteristica,sex);
+        usuario.getPersona().ejecutarTransaccion(new RegistrarMascota(usuario.getPersona(),tip,nombre,apodo,dt,desc,fotos,caracteristica,sex));
 
         Map<String,Object> parametros = new HashMap<>();
 
-        response.redirect("/");
+        response.redirect("/");  //hay que ver como era el redirect
         return new ModelAndView(parametros,"index.hbs");
     }
 }
