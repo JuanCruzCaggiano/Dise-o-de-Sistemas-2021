90d76ec1c7845099d96da58986245dfab1ef21bd
Pdorr <dorrsito@gmail.com>
2021-10-12 21:04:19 -0300

    agrego manejo de fotos

--- src/main/java/dds/domain/controllers/ControllerRegistroMascota.java
+++ src/main/java/dds/domain/controllers/ControllerRegistroMascota.java
@@ -9,44 +9,97 @@ import dds.domain.entities.persona.TipoDocumento;
 import dds.domain.entities.persona.transaccion.RegistrarMascota;
 import dds.domain.entities.seguridad.usuario.Standard;
 import dds.domain.entities.seguridad.usuario.Usuario;
+import org.apache.commons.io.IOUtils;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletException;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static spark.Spark.staticFiles;
 
 public class ControllerRegistroMascota {
+    public static final int DEFAULT_BUFFER_SIZE = 8192;
+
     public ControllerRegistroMascota() {
     }
+    private static String convertInputStreamToString(InputStream is) throws IOException {
 
-    public Response registrarMascota(Request request, Response response) throws NoSuchAlgorithmException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+        int length;
+        while ((length = is.read(buffer)) != -1) {
+            result.write(buffer, 0, length);
+        }
+
+        // Java 1.1
+        return result.toString(StandardCharsets.UTF_8.name());
+
+    }
+    public Response registrarMascota(Request request, Response response) throws NoSuchAlgorithmException, IOException {
         Usuario usuario = request.session().attribute("usuario");
         Asociacion asoc = usuario.getAsociacion();
-        String tipo = (request.queryParams("tipo") != null) ? request.queryParams("tipo") : "";
-        String nombre = (request.queryParams("nombre") != null) ? request.queryParams("nombre") : "";
-        String desc = (request.queryParams("desc") != null) ? request.queryParams("desc") : "";
-        String apodo  = (request.queryParams("apodo") != null) ? request.queryParams("apodo") : "";
-        String sexo = (request.queryParams("sexo") != null) ? request.queryParams("sexo") : "";
-        String caracs = (request.queryParams("caracteristicas") != null) ? request.queryParams("caracteristicas") : "";
-        String foto = (request.queryParams("fotos") != null) ? request.queryParams("fotos") : "";
-        String fecha = (request.queryParams("fecha") != null) ? request.queryParams("fecha") : "";
-        List<String> fotos = new ArrayList<>(); // TODO: Manejo de fotos
+        List<String> preguntasCaracs = asoc.getConfigurador().getClaves();
+        HashMap <String, String> caracteristica = new HashMap<>();
+        File uploadDir = new File("upload");
+
+
+        String tipo=null;
+        String nombre=null;
+        String desc=null;
+        String apodo=null;
+        String sexo = null;
+        String caracs=null;
+        String foto=null;
+        String fecha = null;
+        List<String> fotos = new ArrayList<>();
+        request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+        Path tempFile = Files.createTempFile(uploadDir.toPath(), "", "");
+        try (InputStream input = request.raw().getPart("nombre").getInputStream()) { // getPart needs to use same "name" as input field in form
+            //Files.copy(input, tempFile, StandardCopyOption.REPLACE_EXISTING);
+            String resultado = convertInputStreamToString(input);
+            nombre = resultado;
+            InputStream ss = request.raw().getPart("tipo").getInputStream();
+            tipo=convertInputStreamToString(ss);
+            ss = request.raw().getPart("apodo").getInputStream();
+            apodo = convertInputStreamToString(ss);
+            ss = request.raw().getPart("desc").getInputStream();
+            desc = convertInputStreamToString(ss);
+            ss = request.raw().getPart("sexo").getInputStream();
+            sexo = convertInputStreamToString(ss);
+            for (int i =0 ;i<preguntasCaracs.size();i++){
+                ss = request.raw().getPart(preguntasCaracs.get(i)).getInputStream();
+                caracteristica.put(preguntasCaracs.get(i),convertInputStreamToString(ss));
+            }
+            ss = request.raw().getPart("fecha").getInputStream();
+            fecha = convertInputStreamToString(ss);
+            ss = request.raw().getPart("foto").getInputStream();
+            byte[] bytes = IOUtils.toByteArray(ss);
+            foto = Base64.getEncoder().encodeToString(bytes);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+
         Sexo sexoEnum= Sexo.valueOf(sexo);
         TipoMascota tipoEnum = TipoMascota.valueOf(tipo);
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate dt = LocalDate.parse(fecha,dtf);
-        HashMap <String, String> caracteristica = new HashMap<>();
-        List<String> preguntasCaracs = asoc.getConfigurador().getClaves();
-        for (int i =0 ;i<preguntasCaracs.size();i++){
-            caracteristica.put(preguntasCaracs.get(i),(request.queryParams(preguntasCaracs.get(i)) != null) ? request.queryParams(preguntasCaracs.get(i)) : "");
-        }
+        //fotos.add(foto);
+
+
         //Mascota mascota = new Mascota(tip,nombre,apodo,dt,desc,fotos,caracteristica,sex);
         usuario.getPersona().ejecutarTransaccion(new RegistrarMascota(usuario.getPersona(),tipoEnum,nombre,apodo,dt,desc,fotos,caracteristica,sexoEnum));
 
